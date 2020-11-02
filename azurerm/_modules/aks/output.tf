4265b69011c3986066c57fbb1d6aca0a19947ed5
Dan Feeney <DFeeney@nccdglobal.org>
2020-11-02 11:11:58 -0600

    added kube_config output

--- azurerm/_modules/aks/output.tf
+++ azurerm/_modules/aks/output.tf
@@ -1,3 +1,7 @@
 output "aks_vnet" {
   value = length(azurerm_virtual_network.current) > 0 ? azurerm_virtual_network.current[0] : null
 }
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.current.kube_config
+}
--- azurerm/cluster/output.tf
+++ azurerm/cluster/output.tf
@@ -2,6 +2,10 @@ output "aks_vnet" {
   value = module.cluster.aks_vnet
 }
 
+output "kube_config" {
+  value = module.cluster.kube_config
+}
+
 output "current_config" {
   value = module.configuration.merged[terraform.workspace]
 }
